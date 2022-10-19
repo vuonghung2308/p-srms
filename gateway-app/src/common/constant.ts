5d5ad357d546556d92e8b7f767f4661726337884
Manh Hung <vuonghung2308@gmail.com>
2022-10-19 08:35:03 +0700

    rename folder p-network to nework

--- gateway-app/src/common/constant.ts
+++ gateway-app/src/common/constant.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-export const CCP_PATH = "/home/ronin/p-srms/p-network/"
+export const CCP_PATH = "/home/ronin/p-srms/network/"
     + "organizations/peerOrgs/org1.example.com/connection.json";
 export const CA_HOSTNAME = "ca.org1.example.com";
 export const WALLET_PATH = path.resolve(__dirname, '..', '..', 'wallet');
